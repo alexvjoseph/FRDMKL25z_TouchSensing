d225d2b6e1b5a1242cbf9342572376b1bef4fe5c
Alex Joseph <Alex Joseph>
2016-03-24 18:52:52 -0400

    First Commit

---
+++ Tchpd/FLASH/Sources/TSS/TSS_Sensor.d
@@ -0,0 +1,73 @@
+Sources/TSS/TSS_Sensor.o: ../Sources/TSS/TSS_Sensor.c \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Sources/TSS/TSS_SensorGPIO.h ../Sources/TSS/TSS_Sensor.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Sources/derivative.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/Cpu.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Error.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Const.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/IO_Map.h \
+ ../Sources/TSS/TSS_API.h \
+ C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Sources/TSS_SystemSetup.h \
+ ../Sources/TSS/TSS_StatusCodes.h ../Sources/TSS/TSS_DataTypes.h \
+ ../Sources/TSS/TSS_SensorTSI.h ../Sources/TSS/TSS_SensorTSIL.h
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Sources/TSS/TSS_SensorGPIO.h:
+
+../Sources/TSS/TSS_Sensor.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Sources/derivative.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/Cpu.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Error.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/PE_Const.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Generated_Code/IO_Map.h:
+
+../Sources/TSS/TSS_API.h:
+
+C:/Users/Alex/Desktop/Embedded/CodeWarrior/Tchpd/Sources/TSS_SystemSetup.h:
+
+../Sources/TSS/TSS_StatusCodes.h:
+
+../Sources/TSS/TSS_DataTypes.h:
+
+../Sources/TSS/TSS_SensorTSI.h:
+
+../Sources/TSS/TSS_SensorTSIL.h:
